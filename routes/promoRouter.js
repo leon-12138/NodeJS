cacfa792074755a85496d23a5aea7e62149c617c
Leon <lcai1@ualberta.ca>
2021-07-26 16:00:13 -0400

    HTTPS

--- routes/promoRouter.js
+++ routes/promoRouter.js
@@ -17,7 +17,7 @@ promoRouter.route('/')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.post(authenticate.verifyUser, (req,res,next) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
     Promotions.create(req.body)
     .then((promotion) => {
         console.log('Promotion created ', promotion);
@@ -26,11 +26,11 @@ promoRouter.route('/')
         res.json(promotion);
     }, (err) => next(err))
     .catch((err) => next(err));})
-.put(authenticate.verifyUser, (req,res,next) => {
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
     res.statusCode = 403;
     res.end('PUT operation not supported on /promotions');
 })
-.delete(authenticate.verifyUser, (req,res,next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req,res,next) => {
     Promotions.deleteMany({})
     .then((resp) => {
         res.statusCode = 200;
@@ -48,11 +48,11 @@ promoRouter.route('/:promoId')
         res.json(promo);
     }, (err) => next(err))
     .catch((err) => next(err));})
-.post(authenticate.verifyUser, (req, res, next) => {
+.post(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
   res.statusCode = 403;
   res.end('POST operation not supported on /promotions/'+ req.params.promoId);
 })
-.put(authenticate.verifyUser, (req, res, next) => {
+.put(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Promotions.findByIdAndUpdate(req.params.promoId, {
         $set: req.body
     }, { new: true })
@@ -63,7 +63,7 @@ promoRouter.route('/:promoId')
     }, (err) => next(err))
     .catch((err) => next(err));
 })
-.delete(authenticate.verifyUser, (req, res, next) => {
+.delete(authenticate.verifyUser, authenticate.verifyAdmin, (req, res, next) => {
     Promotions.findByIdAndRemove(req.params.promoId)
     .then((resp) => {
         res.statusCode = 200;
